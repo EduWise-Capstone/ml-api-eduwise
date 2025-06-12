b66cd2b538d8f12dbbe1e4747a914399beaad025
ardiiiv <ardiiiv222@gmail.com>
2025-06-13 01:29:20 +0700

    revisi kedua

--- api.py
+++ api.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 
 CORS(app,
      origins=["https://eduwise-ten.vercel.app"],
-     methods=["GET", "POST", "OPTIONS"]) 
+     methods=["GET", "POST", "OPTIONS"],
+     allow_headers=["Content-Type"],
+     supports_credentials=True)
+ 
 # Load model
 model = load_model('model_h5.h5')
 
